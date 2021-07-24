da4155371adfab9f0b400a3a82b393be135bf244
michellenicole23 <64987167+michellenicole23@users.noreply.github.com>
2021-07-24 14:27:25 -0500

    changed inputbox to functional component with state hooks, changed input-box to tsx

---
+++ src/components/inputBox.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+<h1>Hello</h1>
+
+function InputBox (props: any) {
+
+     const [input, setInput] = useState('Insert Zoom Chat Here');
+      
+     const handleChange = (event: any) => {
+          setInput(input);
+     }
+     const handleSubmit = (event: any) => {
+          alert('A Zoom Chat Was Submitted: ' + input);
+          event.preventDefault();
+     }
+          return (
+               <form onSubmit={handleSubmit}>
+               <label>
+                    Zoom Chat:
+               </label>
+               <br/>
+               <textarea 
+                    value={input} 
+                    onChange={handleChange} />
+               <br/>
+               <input type="submit" value="Submit" />
+          </form>
+          )
+}
+export default InputBox;
