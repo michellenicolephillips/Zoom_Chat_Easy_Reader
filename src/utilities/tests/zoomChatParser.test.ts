08b6863455b4f5ee8451e84f9be40a6cd0ed9983
Bentley Davis <Bentley@BentleyDavis.com>
2021-07-28 12:19:44 -0500

    zoomchatparser now loops

--- src/utilities/tests/zoomChatParser.test.ts
+++ src/utilities/tests/zoomChatParser.test.ts
@@ -14,3 +14,37 @@ it("single message returns one object", () => {
       message: "This is the message",
     }]);
 });
+
+it("two message returns two objects", () => {
+  expect(zoomChatParser(`
+00:00:00 From  John Doe  to  Everyone : This is the first message
+00:00:01 From  John Doe  to  Everyone : This is the second message`))
+    .toMatchObject([
+      {
+        when: "00:00:00",
+        from: "John Doe",
+        to: "Everyone",
+        message: "This is the first message",
+      },
+      {
+        when: "00:00:01",
+        from: "John Doe",
+        to: "Everyone",
+        message: "This is the second message",
+      }
+    ]);
+});
+
+it("message with a line break", () => {
+  expect(zoomChatParser(`
+00:00:00 From  John Doe  to  Everyone : This is the first 
+message`))
+    .toMatchObject([
+      {
+        when: "00:00:00",
+        from: "John Doe",
+        to: "Everyone",
+        message: "This is the first \nmessage",
+      }
+    ]);
+});
--- src/utilities/zoomChatParser.ts
+++ src/utilities/zoomChatParser.ts
@@ -7,17 +7,32 @@ export interface ZoomChat {
 
 export function zoomChatParser(chatText: string): Array<ZoomChat> {
     const messages: Array<ZoomChat> = [];
-    let results = chatText.matchAll(/(\d\d:\d\d:\d\d) From\s{1,2}(.*?)\s{1,2}to\s{1,2}(.*?)\s{1,2}:\s{1,2}(.*)/gm);
-    const result = results.next();
-    if (chatText == "") {
-        return messages;
+    let matches = chatText.matchAll(/(\d\d:\d\d:\d\d) From\s{1,2}(.*?)\s{1,2}to\s{1,2}(.*?)\s{1,2}:\s{1,2}/gm);
+
+    let lastMatch;
+    let lastMessage: ZoomChat | undefined;
+
+    for (const match of matches) {
+        const newMesage: any = {
+            when: match[1],
+            from: match[2],
+            to: match[3],
+            message: match[4],
+        };
+        messages.push(newMesage);
+
+        if (lastMatch && lastMessage && lastMatch.input?.length && lastMatch.index && match.index) {
+            lastMessage.message = chatText.substring(lastMatch.index + lastMatch[0].length, match.index - 1)
+        }
+
+        lastMatch = match;
+        lastMessage = newMesage
     }
-    const zoomChat: any = {
-        when: result.value[1],
-        from: result.value[2],
-        to: result.value[3],
-        message: result.value[4],
-    };
-    messages.push(zoomChat);
+
+    if (lastMatch !== undefined && lastMessage !==undefined && lastMatch.input?.length !== undefined && lastMatch.index !== undefined) {
+        lastMessage.message = chatText.substring(lastMatch.index + lastMatch[0].length)
+    }
+
+
     return messages;
 };
