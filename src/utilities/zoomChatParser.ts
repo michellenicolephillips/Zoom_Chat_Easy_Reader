332211d3a7d3f7e750b79298809bca985cc94384
michellenicole23 <64987167+michellenicole23@users.noreply.github.com>
2021-07-28 09:19:37 -0500

    added if statement to address empty zoomchat, completed results regex to match message

--- src/utilities/zoomChatParser.ts
+++ src/utilities/zoomChatParser.ts
@@ -7,16 +7,16 @@ export interface ZoomChat {
 
 export function zoomChatParser(chatText: string): Array<ZoomChat> {
     const messages: Array<ZoomChat> = [];
-    let results = chatText.matchAll(/(\d\d:\d\d:\d\d) From\s{1,2}(.*?)\s{1,2}to\s{1,2}(.*?)\s{1,2}:/gm);
+    let results = chatText.matchAll(/(\d\d:\d\d:\d\d) From\s{1,2}(.*?)\s{1,2}to\s{1,2}(.*?)\s{1,2}:\s{1,2}(.*)/gm);
     const result = results.next();
-    // let from = chatText.match(/ From\s{1,2}(.*?)\s{1,2}.(?=to)/);
-    // let to = chatText.match(/Everyone/);
-    // let message = chatText.match(/:\s{1,2}.*/);
+    if (chatText == "") {
+        return messages;
+    }
     const zoomChat: any = {
         when: result.value[1],
         from: result.value[2],
         to: result.value[3],
-        //message: message,
+        message: result.value[4],
     };
     messages.push(zoomChat);
     return messages;
